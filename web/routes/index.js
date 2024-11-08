e2429a3e9ac8242f8b78beb79bbfec20fbe13562
Adrian-Rodriguez-Reyes <adrian.s370407@cesurformacion.com>
2024-11-08 11:40:34 +0100

    finalizado

--- web/routes/index.js
+++ web/routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
-
+var datos = require("../data/dataprovider");
+const {getPostById} = require("../data/dataprovider");
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'RetroBlog', categorias: datos.getCategories(), imagenes: datos.getAllPosts() });
+});
+
+router.get('/pelicula/:id', function(req, res, next) {
+  var pelicula = datos.getPostById(req.params.id);
+  res.render('pelicula', { title: 'RetroBlog', pelicula: pelicula, categorias: datos.getCategories()});
 });
 
 module.exports = router;
